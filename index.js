5a619a4443406bbc3f5c2f4a6670594b39756cb4
ssagar7007 <ssagargupta7007@gmail.com>
2022-07-14 20:05:23 +0530

    make it better

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/',(req, res) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose
-  .connect(process.env.CONNECTION_URL)
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() =>
     app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
   )
