1f5e78296d53d678da2e730e6f2fab3d7d4de45b
ssagar7007 <ssagargupta7007@gmail.com>
2022-07-14 17:33:15 +0530

    make it better

--- index.js
+++ index.js
@@ -17,12 +17,12 @@ app.get('/',(req, res) => {
   res.send('Hello to crud app API')
 })
 
-// const PORT = process.env.PORT||5000;
+const PORT = process.env.PORT||5000;
 
 mongoose
   .connect(process.env.CONNECTION_URL)
   .then(() =>
-    app.listen(process.env.PORT, () => console.log(`Server running on port : ${process.env.PORT}`))
+    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
   )
   .catch((error) => console.log(error.message));
 
