946034a4c2820dfddc5c6ddafea0391311115181
ssagar7007 <ssagargupta7007@gmail.com>
2022-07-14 20:21:59 +0530

    make it better

--- index.js
+++ index.js
@@ -20,11 +20,23 @@ app.get('/',(req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-mongoose
-  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() =>
-    app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
-  )
-  .catch((error) => console.log(error.message));
+// mongoose
+//   .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//   .then(() =>
+//     app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
+//   )
+//   .catch((error) => console.log(error.message));
 
 // mongoose.set('useFindAndModify', false);
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const client = new MongoClient(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
