a1e3097406bff0e33e5442423576153552a5cc65
ssagar7007 <ssagargupta7007@gmail.com>
2022-07-14 20:35:38 +0530

    make it better

--- index.js
+++ index.js
@@ -18,25 +18,14 @@ app.get('/',(req, res) => {
   res.send('Hello to crud app API')
 })
 
-const PORT = process.env.PORT || 5000;
+const port = app.listen(process.env.PORT || 5000);
 
-// mongoose
-//   .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-//   .then(() =>
-//     app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))
-//   )
-//   .catch((error) => console.log(error.message));
+mongoose
+  .connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(port, () => console.log(`Server running on port : ${port}`))
+  )
+  .catch((error) => console.log(error.message));
 
 // mongoose.set('useFindAndModify', false);
-
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-
-const client = new MongoClient(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
