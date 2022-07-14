a5fa6b528215aa8895bbb038aec173ab4c5ca508
ssagar7007 <ssagargupta7007@gmail.com>
2022-07-14 19:49:28 +0530

    make it better

--- index.js
+++ index.js
@@ -13,11 +13,12 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 app.use("/posts", postRoutes);
+
 app.get('/',(req, res) => {
   res.send('Hello to crud app API')
 })
 
-const PORT = process.env.PORT||5000;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.CONNECTION_URL)
@@ -26,4 +27,4 @@ mongoose
   )
   .catch((error) => console.log(error.message));
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', false);
